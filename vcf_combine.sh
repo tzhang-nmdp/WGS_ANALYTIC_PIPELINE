658c24b38bcc5396910f01bbac993cb7d637ea72
taozhang2019 <56171080+taozhang2019@users.noreply.github.com>
2020-04-24 10:59:10 -0500

    Update vcf_combine.sh

--- vcf_combine.sh
+++ vcf_combine.sh
@@ -2,9 +2,14 @@
 outdir=$1
 sample=$2
 
+# sort  and compress the vcf by coordinates
 (grep ^"#" ${outdir}/${sample}.snpEff.vcf ; grep -v ^"#" ${outdir}/${sample}.snpEff.vcf \
 | sort -T ${HOME}/tmp -k1,1 -k2,2n) \ 
 | bgzip -c -f \
 > ${outdir}/${sample}.snpEff.vcf.gz
+
+# index the vcf by coordinates
 tabix -p vcf -f ${outdir}/${sample}.snpEff.vcf.gz
+
+# make vcf file list
 ls ${outdir}/${sample}.snpEff.vcf.gz >> ${outdir}/${sample_list}.snpeff
