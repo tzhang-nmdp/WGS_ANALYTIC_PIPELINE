18e8eba742a19fe31be7330ba5e24c93291b1587
taozhang2019 <56171080+taozhang2019@users.noreply.github.com>
2020-04-24 10:57:27 -0500

    Update vcf_combine.sh

--- vcf_combine.sh
+++ vcf_combine.sh
@@ -2,6 +2,9 @@
 outdir=$1
 sample=$2
 
-(grep ^"#" ${outdir}/${sample}.snpEff.vcf ; grep -v ^"#" ${outdir}/${sample}.snpEff.vcf | sort -T ${HOME}/tmp -k1,1 -k2,2n) | bgzip -c -f > ${outdir}/${sample}.snpEff.vcf.gz
+(grep ^"#" ${outdir}/${sample}.snpEff.vcf ; grep -v ^"#" ${outdir}/${sample}.snpEff.vcf \
+| sort -T ${HOME}/tmp -k1,1 -k2,2n) \ 
+| bgzip -c -f \
+> ${outdir}/${sample}.snpEff.vcf.gz
 tabix -p vcf -f ${outdir}/${sample}.snpEff.vcf.gz
 ls ${outdir}/${sample}.snpEff.vcf.gz >> ${outdir}/${sample_list}.snpeff
