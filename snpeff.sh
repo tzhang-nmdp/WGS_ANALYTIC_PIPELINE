e0a6a7fe52debc0d73736fb879904fc19fd966db
taozhang2019 <56171080+taozhang2019@users.noreply.github.com>
2020-04-24 10:34:40 -0500

    Update snpeff.sh

--- snpeff.sh
+++ snpeff.sh
@@ -8,6 +8,9 @@ mkdir ${outdir}/snpEff_vcf
 # 2. snpEff basic variant annotation
 echo -e "${vcf} snpEff startTime::::::::\c"; date
 
-java -d64 -Xmx3g -jar ${HOME}/software/snpEff/snpEff.jar -v GRCh38.86 ${outdir}/snpEff_vcf/${sample}.filtered.vcf -canon -stats ${outdir}/snpEff_vcf/${sample}.html > ${outdir}/snpEff_vcf/${sample}.snpEff.vcf
+java -d64 -Xmx3g -jar ${HOME}/software/snpEff/snpEff.jar \
+-v GRCh38.86 ${outdir}/snpEff_vcf/${sample}.filtered.vcf \
+-canon -stats ${outdir}/snpEff_vcf/${sample}.html \
+> ${outdir}/snpEff_vcf/${sample}.snpEff.vcf
 
 echo -e "${vcf} snpEff endTime::::::::\c"; date
