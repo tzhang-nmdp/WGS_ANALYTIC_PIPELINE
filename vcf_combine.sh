f7a1597738e9e6684bd47c0da5c68268f012ff75
taozhang2019 <56171080+taozhang2019@users.noreply.github.com>
2020-04-30 16:01:02 -0500

    Update vcf_combine.sh

--- vcf_combine.sh
+++ vcf_combine.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 outdir=$1
 sample=$2
-HOME=/home/tzhang
+sample_list=$3
+HOME=$(awk '($1~"^HOME"){print $0}' cfg | cut -d "=" -f 2)
 
 # sort  and compress the vcf by coordinates
-(grep ^"#" ${outdir}/${sample}.snpEff.vcf ; grep -v ^"#" ${outdir}/${sample}.snpEff.vcf \
-| sort -T ${HOME}/tmp -k1,1 -k2,2n) \ 
-| bgzip -c -f \
-> ${outdir}/${sample}.snpEff.vcf.gz
+echo ${sample}
+(grep ^"#" ${outdir}/snpEff_vcf/${sample}.snpEff.vcf ; \
+grep -v ^"#" ${outdir}/snpEff_vcf/${sample}.snpEff.vcf \
+| sort -T ${HOME}/tmp -k1,1 -k2,2n) | bgzip -c -f \
+> ${outdir}/snpEff_vcf/${sample}.snpEff.vcf.gz
 
 # index the vcf by coordinates
-tabix -p vcf -f ${outdir}/${sample}.snpEff.vcf.gz
+tabix -p vcf -f ${outdir}/snpEff_vcf/${sample}.snpEff.vcf.gz
 
 # make vcf file list
-ls ${outdir}/${sample}.snpEff.vcf.gz >> ${outdir}/${sample_list}.snpeff
+ls ${outdir}/snpEff_vcf/${sample}.snpEff.vcf.gz >> ${outdir}/${sample_list}.snpeff
