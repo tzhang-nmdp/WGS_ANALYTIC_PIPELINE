9ec5aca0f7be9d1a37b66e8651f88ebc298f8681
taozhang2019 <56171080+taozhang2019@users.noreply.github.com>
2020-05-01 09:13:24 -0500

    Update vcf_id_fix.py

--- vcf_id_fix.py
+++ vcf_id_fix.py
@@ -5,10 +5,12 @@ sample_file=open(sys.argv[2],'r')
 output=open(str(sys.argv[1])+'.cr','r')
 sample_list=[]
 
+# generate sample list
 for line in sample_file:
     item=line.strip().split('\t')  
     sample_list.append(item[0])  
         
+# fix sample id in vcf        
 for line in input_vcf:
     item=line.strip().split('\t')  
     if item[0].startswith('#CHROM'):
